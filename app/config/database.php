9209d42d0274abbea371be3b724ae00eb59251e1
Waleed Ahmad <waleedgplus@gmail.com>
2015-08-23 14:06:34 -0400

    admin approval logic for authentication

--- app/config/database.php
+++ app/config/database.php
@@ -57,7 +57,7 @@ return array(
 			'host'      => '127.0.0.1',
 			'database'  => 'levelgauging',
 			'username'  => 'root',
-			'password'  => '',
+			'password'  => 'binarystar',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',

2991a54d053a06eee79831046daa5b40030fedd2
Waleed Ahmad <waleedgplus@gmail.com>
2015-09-21 14:01:22 +0500

    Added bower dependencies and fixes asset links

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -8,9 +8,9 @@
 			href="//f.fontdeck.com/s/css/sDLal4th9BQH2lvZFncwJ6sQBs4/{{$_SERVER['SERVER_NAME']}}/59246.css"
 			type="text/css"
 		/>
-		<link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
-		<link rel="stylesheet" href="{{URL::to('/assets/')}}/bower_components/bootstrap/dist/css/bootstrap.min.css">
-		<link rel="stylesheet" href="{{URL::to('/assets/build/css/style.css')}}">
+		<link rel="stylesheet" href="/assets/bower_components/jquery-ui/themes/smoothness/jquery-ui.min.css">
+		<link rel="stylesheet" href="/assets/bower_components/bootstrap/dist/css/bootstrap.min.css">
+		<link rel="stylesheet" href="/assets/build/css/style.css">
 		@if(isset($tank->id))
 		<meta name="tankid" content="{{$tank->id}}">
 		@endif
@@ -31,9 +31,9 @@
 			@yield('admin-authorize')
 		</div>
 	</body>
-	<script type="text/javascript" src="{{URL::to('/assets/')}}/bower_components/jquery/dist/jquery.min.js"></script>
-	<script type="text/javascript" src="{{URL::to('/assets/')}}/bower_components/jquery-ui/ui/minified/datepicker.min.js"></script>
-	<script type="text/javascript" src="{{URL::to('/assets/')}}/build/js/timepicker.min.js"></script>
-	<script type="text/javascript" src="{{URL::to('/assets/')}}/build/js/app.min.js"></script>
+	<script type="text/javascript" src="/assets/bower_components/jquery/dist/jquery.min.js"></script>
+	<script type="text/javascript" src="/assets/bower_components/jquery-ui/ui/minified/datepicker.min.js"></script>
+	<script type="text/javascript" src="/assets/build/js/timepicker.min.js"></script>
+	<script type="text/javascript" src="/assets/build/js/app.min.js"></script>
 
 </html>
